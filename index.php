f59466d6d74f31cc0f8fef89582fe18789f942cb
ziaaflame <38154276+ziaaflame@users.noreply.github.com>
2018-07-27 11:16:01 +0600

    php7

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+    $array = ['Rasel', 'Ahmed', 35];
+    echo $array[1]; echo '<br/>';
+
+    $name = 'tarin';
+
+    if($name === 'tarin') {
+        echo 'she is a student';
+    } elseif(#name === 'tarina) {
+        echo 'she is no a student';
+    } else {
+        echo 'she is a teacher';
+    }
+?>
